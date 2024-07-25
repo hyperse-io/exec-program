77dda756553c3074320e3ad4d62d2060b9bba71b
tianyingchun <tianyingchun@outlook.com>
2024-07-25 12:08:51 +0800

    chore: bump version

--- tests/exec.spec.ts
+++ tests/exec.spec.ts
@@ -1,6 +1,6 @@
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { runTsScript } from '../src/runTsScript.js';
+import { runTsScript } from '@/runTsScript.js';
 
 const getDirname = (url: string, ...paths: string[]) => {
   return join(dirname(fileURLToPath(url)), ...paths);
